6eb5ea7ec41940eabe69c0efb8fb60edd2ba5c5f
Karl Thomas Hauglid <thomas.hauglid@gmail.com>
2022-11-23 13:00:31 +0100

    add action to store details

--- Find A Coop/Features/Stores/Controllers/StoreInfo/StoreInfoViewController.swift
+++ Find A Coop/Features/Stores/Controllers/StoreInfo/StoreInfoViewController.swift
@@ -7,18 +7,20 @@
 
 import Foundation
 import UIKit
+import SafariServices
 
 class StoreInfoViewController: UIViewController{
     var store: Store?
     
     @IBOutlet weak var openNowLabel: UILabel!
     @IBOutlet weak var storeNameLabel: UILabel!
-    
+    @IBOutlet weak var openingHoursTodayLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var phoneLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var openingHoursStack: UIStackView!
-    @IBOutlet weak var openingHoursTodayLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         
         guard let store = self.store else { return  }
@@ -46,6 +48,59 @@ class StoreInfoViewController: UIViewController{
             label.text = "\(openingHour.day) \(openingHour.openString)"
             self.openingHoursStack.addArrangedSubview(label)
         }
+    }
+    
+    
+    @IBAction func phoneTap(_ sender: Any) {
+        guard let phoneNumber = store?.phone else { return  }
+        guard let url = URL(string: "telprompt://\(phoneNumber)"),
+              UIApplication.shared.canOpenURL(url) else {
+            return
+        }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func emailTap(_ sender: Any) {
+        guard let email = store?.email else { return  }
+        guard let url = URL(string: "mailto:\(email)"),
+              UIApplication.shared.canOpenURL(url) else {
+            return
+        }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func addressTap(_ sender: Any) {
+        guard let store = store else { return  }
+        
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "google.com"
+        components.path = "/maps/search/"
+
+        components.queryItems = [
+            URLQueryItem(name: "api", value: "1"),
+            URLQueryItem(name: "query", value: "\(store.chain) \(store.name) \(store.address)"),
+            
+        ]
+        guard let url = components.url,
+              UIApplication.shared.canOpenURL(url) else {
+            print("Cannot open map")
+            return
+        }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    @IBAction func offerTap(_ sender: Any) {
+        guard let store = store else { return  }
+        
+        
+        guard let url = URL(string: store.newspaperUrl),
+              UIApplication.shared.canOpenURL(url) else {
+            print("Cannot open offer")
+            return
+        }
+        let svc = SFSafariViewController(url: url)
+        present(svc, animated: true, completion: nil)
     }
     
 }
